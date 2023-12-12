27a5859c274f0dd592dfc55b6769f0e8684fa07b
Young-Chung Hsue <hsuy1234@hotmail.com>
2023-12-12 16:26:26 +0800

    補充 fileExporter的定義

--- dts/forFileExporter.d.ts
+++ dts/forFileExporter.d.ts
@@ -1,4 +1,13 @@
 ///<reference path="common.d.ts"/>
 
+export interface FileExporterAPI {
+    files: () => Array<string>;
+    remove: (filename: string) => void;
+    write: (filename: string, content: string) => void;
+    read: (filename: string) => string;
+}
+
+export var api: FileExporterAPI;
+
 export var input: { src: string, dst: string }[];
 export var output: { src: string, dst: string }[];
